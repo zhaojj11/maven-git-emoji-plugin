c2088e6cb6750d1f2ce3f88e680ec88ac4e1be3c
zhaojj11 <zhaojj11@zhaojj11.com>
2022-05-05 22:44:19 +0800

    :sparkles: feat: add commit-msg, pre-commit hooks

---
+++ src/main/java/com/zhaojj11/plugins/PreCommitMojo.java
@@ -0,0 +1,36 @@
+package com.zhaojj11.plugins;
+
+import com.zhaojj11.plugins.util.UsageUtil;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+
+/**
+ * pre-commit hook
+ *
+ * @author zhaojj11
+ * @since 1.0.0
+ */
+@Mojo(name = "pre-commit", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresDependencyResolution = ResolutionScope.NONE)
+public class PreCommitMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject project;
+
+    @Parameter(defaultValue = "${project.basedir}", readonly = true)
+    private File basedir;
+
+    @Parameter(defaultValue = "${plugin}", readonly = true)
+    private PluginDescriptor pluginDescriptor;
+
+    @Override
+    public void execute() {
+        UsageUtil.logHelpUsage(getLog(), project, pluginDescriptor, basedir);
+    }
+}
